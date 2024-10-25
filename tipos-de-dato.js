501821bb5e2ec25fe81c8f552d0f3c4a77bbc372
DBC <chinobc20012@gmail.com>
2024-10-25 12:34:32 -0600

    tipos de datos en javascript

---
+++ tipos-de-dato.js
@@ -0,0 +1,14 @@
+console.log (typeof 150)
+
+console.log (typeof "saludos cordiales")
+
+console.log (typeof true)
+console.log (typeof false)
+
+var miVariable = null 
+console.log (miVariable)
+
+var escuadra;
+console.log (typeof escuadra);
+
+console.log (typeof 17/4/63)
